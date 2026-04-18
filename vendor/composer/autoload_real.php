45173442d77bf8343cd29a3a8d9bec443b1e2d3e
Phillip <simontx1983@gmail.com>
2026-04-18 02:30:53 -0500

    Update composer autoloader classmap

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit01a6b85c621fb3373fd6f39353ebbe86
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit01a6b85c621fb3373fd6f39353ebbe86::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
